6fe91f5f79787e09af75aeccd1ef0e2c465c34cb
Julian Mejliker <jmejlker@fi.uba.ar>
2024-04-16 18:29:13 +0800

    Added code to open a udp server on the station when connected

--- 02_AP_station_mode/main/main.c
+++ 02_AP_station_mode/main/main.c
@@ -10,24 +10,29 @@
 /*
     This example shows how to scan for available set of APs.
 */
+#include <lwip/netdb.h>
 #include <string.h>
+
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_wifi.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
-#include "esp_wifi.h"
-#include "esp_log.h"
-#include "esp_event.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
 #include "nvs_flash.h"
 
 #define DEFAULT_SCAN_LIST_SIZE CONFIG_EXAMPLE_SCAN_LIST_SIZE
-#define EXAMPLE_ESP_MAXIMUM_RETRY  10
+#define EXAMPLE_ESP_MAXIMUM_RETRY 10
 static EventGroupHandle_t s_wifi_event_group;
-
 
 /* The event group allows multiple bits for each event, but we only care about two events:
  * - we are connected to the AP with an IP
  * - we failed to connect after the maximum amount of retries */
 #define WIFI_CONNECTED_BIT BIT0
-#define WIFI_FAIL_BIT      BIT1
+#define WIFI_FAIL_BIT BIT1
+#define PORT 3333
 
 static const char *TAG = "station";
 
@@ -35,118 +40,240 @@ static int s_retry_num = 0;
 
 /* Initialize Wi-Fi as sta and set scan method */
 wifi_ap_record_t wifi_scan(void) {
-    uint16_t number = DEFAULT_SCAN_LIST_SIZE;
-    wifi_ap_record_t ap_info[DEFAULT_SCAN_LIST_SIZE];
-    uint16_t ap_count = 0;
-    memset(ap_info, 0, sizeof(ap_info));
+  uint16_t number = DEFAULT_SCAN_LIST_SIZE;
+  wifi_ap_record_t ap_info[DEFAULT_SCAN_LIST_SIZE];
+  uint16_t ap_count = 0;
+  memset(ap_info, 0, sizeof(ap_info));
 
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-    ESP_ERROR_CHECK(esp_wifi_start());
-    esp_wifi_scan_start(NULL, true);
-    ESP_LOGI(TAG, "Max AP number ap_info can hold = %u", number);
-    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&number, ap_info));
-    ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&ap_count));
-    ESP_LOGI(TAG, "Total APs scanned = %u, actual AP number ap_info holds = %u", ap_count, number);
-    uint16_t index_ap = 0;
-    for (int i = 0; i < number; i++) {
-        // Find the AP with the name like 'ESP_'
-        if (strstr((const char *)ap_info[i].ssid, "ESP_") != NULL) {
-            index_ap = i;
-            ESP_LOGI(TAG, "SSID \t\t%s", ap_info[i].ssid);
-            ESP_LOGI(TAG, "RSSI \t\t%d", ap_info[i].rssi);
-            ESP_LOGI(TAG, "Channel \t\t%d", ap_info[i].primary);
-        }
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_start());
+  esp_wifi_scan_start(NULL, true);
+  ESP_LOGI(TAG, "Max AP number ap_info can hold = %u", number);
+  ESP_ERROR_CHECK(esp_wifi_scan_get_ap_records(&number, ap_info));
+  ESP_ERROR_CHECK(esp_wifi_scan_get_ap_num(&ap_count));
+  ESP_LOGI(TAG, "Total APs scanned = %u, actual AP number ap_info holds = %u", ap_count, number);
+  uint16_t index_ap = 0;
+  for (int i = 0; i < number; i++) {
+    // Find the AP with the name like 'ESP_'
+    if (strstr((const char *)ap_info[i].ssid, "ESP_") != NULL) {
+      index_ap = i;
+      ESP_LOGI(TAG, "SSID \t\t%s", ap_info[i].ssid);
+      ESP_LOGI(TAG, "RSSI \t\t%d", ap_info[i].rssi);
+      ESP_LOGI(TAG, "Channel \t\t%d", ap_info[i].primary);
     }
+  }
 
-    return (wifi_ap_record_t) ap_info[index_ap];
+  return (wifi_ap_record_t)ap_info[index_ap];
 }
 
-static void event_handler(void* arg, esp_event_base_t event_base,
-                                int32_t event_id, void* event_data)
-{
-    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-        esp_wifi_connect();
-    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
-            esp_wifi_connect();
-            s_retry_num++;
-            ESP_LOGI(TAG, "retry to connect to the AP");
-        } else {
-            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
-        }
-        ESP_LOGI(TAG,"connect to the AP fail");
-    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
-        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-        s_retry_num = 0;
-        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+static void udp_server_task(void *pvParameters) {
+  char rx_buffer[128];
+  char addr_str[128];
+  int addr_family = (int)pvParameters;
+  int ip_protocol = 0;
+  struct sockaddr_in6 dest_addr;
+
+  while (1) {
+    if (addr_family == AF_INET) {
+      struct sockaddr_in *dest_addr_ip4 = (struct sockaddr_in *)&dest_addr;
+      dest_addr_ip4->sin_addr.s_addr = htonl(INADDR_ANY);
+      dest_addr_ip4->sin_family = AF_INET;
+      dest_addr_ip4->sin_port = htons(PORT);
+      ip_protocol = IPPROTO_IP;
+    } else if (addr_family == AF_INET6) {
+      bzero(&dest_addr.sin6_addr.un, sizeof(dest_addr.sin6_addr.un));
+      dest_addr.sin6_family = AF_INET6;
+      dest_addr.sin6_port = htons(PORT);
+      ip_protocol = IPPROTO_IPV6;
     }
+
+    int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
+    if (sock < 0) {
+      ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+      break;
+    }
+    ESP_LOGI(TAG, "Socket created");
+
+#if defined(CONFIG_LWIP_NETBUF_RECVINFO) && !defined(CONFIG_EXAMPLE_IPV6)
+    int enable = 1;
+    lwip_setsockopt(sock, IPPROTO_IP, IP_PKTINFO, &enable, sizeof(enable));
+#endif
+
+#if defined(CONFIG_EXAMPLE_IPV4) && defined(CONFIG_EXAMPLE_IPV6)
+    if (addr_family == AF_INET6) {
+      // Note that by default IPV6 binds to both protocols, it is must be disabled
+      // if both protocols used at the same time (used in CI)
+      int opt = 1;
+      setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+      setsockopt(sock, IPPROTO_IPV6, IPV6_V6ONLY, &opt, sizeof(opt));
+    }
+#endif
+    // Set timeout
+    struct timeval timeout;
+    timeout.tv_sec = 10;
+    timeout.tv_usec = 0;
+    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof timeout);
+
+    int err = bind(sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+    if (err < 0) {
+      ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
+    }
+    ESP_LOGI(TAG, "Socket bound, port %d", PORT);
+
+    struct sockaddr_storage source_addr;  // Large enough for both IPv4 or IPv6
+    socklen_t socklen = sizeof(source_addr);
+
+#if defined(CONFIG_LWIP_NETBUF_RECVINFO) && !defined(CONFIG_EXAMPLE_IPV6)
+    struct iovec iov;
+    struct msghdr msg;
+    struct cmsghdr *cmsgtmp;
+    u8_t cmsg_buf[CMSG_SPACE(sizeof(struct in_pktinfo))];
+
+    iov.iov_base = rx_buffer;
+    iov.iov_len = sizeof(rx_buffer);
+    msg.msg_control = cmsg_buf;
+    msg.msg_controllen = sizeof(cmsg_buf);
+    msg.msg_flags = 0;
+    msg.msg_iov = &iov;
+    msg.msg_iovlen = 1;
+    msg.msg_name = (struct sockaddr *)&source_addr;
+    msg.msg_namelen = socklen;
+#endif
+
+    while (1) {
+      ESP_LOGI(TAG, "Waiting for data");
+#if defined(CONFIG_LWIP_NETBUF_RECVINFO) && !defined(CONFIG_EXAMPLE_IPV6)
+      int len = recvmsg(sock, &msg, 0);
+#else
+      int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0, (struct sockaddr *)&source_addr, &socklen);
+#endif
+      // Error occurred during receiving
+      if (len < 0) {
+        ESP_LOGE(TAG, "recvfrom failed: errno %d", errno);
+        break;
+      }
+      // Data received
+      else {
+        // Get the sender's ip address as string
+        if (source_addr.ss_family == PF_INET) {
+          inet_ntoa_r(((struct sockaddr_in *)&source_addr)->sin_addr, addr_str, sizeof(addr_str) - 1);
+#if defined(CONFIG_LWIP_NETBUF_RECVINFO) && !defined(CONFIG_EXAMPLE_IPV6)
+          for (cmsgtmp = CMSG_FIRSTHDR(&msg); cmsgtmp != NULL; cmsgtmp = CMSG_NXTHDR(&msg, cmsgtmp)) {
+            if (cmsgtmp->cmsg_level == IPPROTO_IP && cmsgtmp->cmsg_type == IP_PKTINFO) {
+              struct in_pktinfo *pktinfo;
+              pktinfo = (struct in_pktinfo *)CMSG_DATA(cmsgtmp);
+              ESP_LOGI(TAG, "dest ip: %s\n", inet_ntoa(pktinfo->ipi_addr));
+            }
+          }
+#endif
+        } else if (source_addr.ss_family == PF_INET6) {
+          inet6_ntoa_r(((struct sockaddr_in6 *)&source_addr)->sin6_addr, addr_str, sizeof(addr_str) - 1);
+        }
+
+        rx_buffer[len] = 0;  // Null-terminate whatever we received and treat like a string...
+        ESP_LOGI(TAG, "Received %d bytes from %s:", len, addr_str);
+        ESP_LOGI(TAG, "%s", rx_buffer);
+
+        int err = sendto(sock, rx_buffer, len, 0, (struct sockaddr *)&source_addr, sizeof(source_addr));
+        if (err < 0) {
+          ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+          break;
+        }
+      }
+    }
+
+    if (sock != -1) {
+      ESP_LOGE(TAG, "Shutting down socket and restarting...");
+      shutdown(sock, 0);
+      close(sock);
+    }
+  }
+  vTaskDelete(NULL);
 }
 
-void app_main(void){
-
-    s_wifi_event_group = xEventGroupCreate();
-
-    // Initialize NVS
-    esp_err_t ret = nvs_flash_init();
-    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-        ESP_ERROR_CHECK(nvs_flash_erase());
-        ret = nvs_flash_init();
-    }
-    ESP_ERROR_CHECK( ret );
-    
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
-    assert(sta_netif);
-
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-
-    esp_event_handler_instance_t instance_any_id;
-    esp_event_handler_instance_t instance_got_ip;
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
-                                                        ESP_EVENT_ANY_ID,
-                                                        &event_handler,
-                                                        NULL,
-                                                        &instance_any_id));
-    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
-                                                        IP_EVENT_STA_GOT_IP,
-                                                        &event_handler,
-                                                        NULL,
-                                                        &instance_got_ip));
-
-    wifi_ap_record_t record = wifi_scan();
-    ESP_LOGI(TAG, "SSID final \t\t%s", record.ssid);
-
-
-    // Connect to the AP with the record found
-    wifi_config_t wifi_config = {};
-    strcpy((char *)wifi_config.sta.ssid, (const char *)record.ssid);
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
-    ESP_ERROR_CHECK(esp_wifi_connect());
-
-    // Get the IP address of the AP that the station is connected and print it
-    // esp_wifi_sta_get_ap_info(&record);
-
-    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
-     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
-    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-            pdFALSE,
-            pdFALSE,
-            portMAX_DELAY);
-
-    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
-     * happened. */
-    if (bits & WIFI_CONNECTED_BIT) {
-        ESP_LOGI(TAG, "connected to ap SSID:%s",
-                 (const char *)record.ssid);
-    } else if (bits & WIFI_FAIL_BIT) {
-        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
-                 "EXAMPLE_ESP_WIFI_SSID", "EXAMPLE_ESP_WIFI_PASS");
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data) {
+  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+    esp_wifi_connect();
+  } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+    if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY) {
+      esp_wifi_connect();
+      s_retry_num++;
+      ESP_LOGI(TAG, "retry to connect to the AP");
     } else {
-        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+      xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
     }
-    
+    ESP_LOGI(TAG, "connect to the AP fail");
+  } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+    ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+    ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+    s_retry_num = 0;
+    xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    xTaskCreate(udp_server_task, "udp_server", 4096, (void *)AF_INET, 5, NULL);
+  }
+}
+
+void app_main(void) {
+  s_wifi_event_group = xEventGroupCreate();
+
+  // Initialize NVS
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
+  assert(sta_netif);
+
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+  esp_event_handler_instance_t instance_any_id;
+  esp_event_handler_instance_t instance_got_ip;
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                      ESP_EVENT_ANY_ID,
+                                                      &event_handler,
+                                                      NULL,
+                                                      &instance_any_id));
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                      IP_EVENT_STA_GOT_IP,
+                                                      &event_handler,
+                                                      NULL,
+                                                      &instance_got_ip));
+
+  wifi_ap_record_t record = wifi_scan();
+  ESP_LOGI(TAG, "SSID final \t\t%s", record.ssid);
+
+  // Connect to the AP with the record found
+  wifi_config_t wifi_config = {};
+  strcpy((char *)wifi_config.sta.ssid, (const char *)record.ssid);
+  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_connect());
+
+  // Get the IP address of the AP that the station is connected and print it
+  // esp_wifi_sta_get_ap_info(&record);
+
+  /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+   * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+  EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+                                         WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                         pdFALSE,
+                                         pdFALSE,
+                                         portMAX_DELAY);
+
+  /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+   * happened. */
+  if (bits & WIFI_CONNECTED_BIT) {
+    ESP_LOGI(TAG, "connected to ap SSID:%s",
+             (const char *)record.ssid);
+  } else if (bits & WIFI_FAIL_BIT) {
+    ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+             "EXAMPLE_ESP_WIFI_SSID", "EXAMPLE_ESP_WIFI_PASS");
+  } else {
+    ESP_LOGE(TAG, "UNEXPECTED EVENT");
+  }
 }
